f6fd61a63d961ba1ececc0e9801e00e55811aaaf
angel <lonnie.hughes@astontech.com>
2021-10-29 17:52:49 -0500

    added form control options, removing old nav exmpl

--- src/index.js
+++ src/index.js
@@ -2,13 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.component';
-import {BrowserRouter} from 'react-router-dom';
+// import {BrowserRouter} from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+//import the reducer to be added to combine reducers
+import { buttonOneReducer, userReducer } from './redux/reducer';
+
+//import redux logger for logging of reducer actions
+import logger from 'redux-logger';
+
+
+const storeInstance = createStore (
+  combineReducers({
+    buttonOneReducer, userReducer
+  }),
+  applyMiddleware(logger)
+);
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={storeInstance}>
     <App />
-  </BrowserRouter>,
-    
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/redux/constants.js
@@ -0,0 +1,3 @@
+//using a constant in order to not mess up with string spelling errors
+export const ADD_BUTTON_ONE = "ADD_BUTTON_ONE";
+export const SUBMIT_FORM = 'SUBMIT_FORM';
